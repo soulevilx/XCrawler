01ebac09ef4810b49d2c4c668ecf6cfe39fd8d1b
Viet Vu <jooservices@gmail.com>
2023-04-23 18:23:37 +0700

    Bugs fixing

--- app/Modules/Flickr/Jobs/Queues/Owner.php
+++ app/Modules/Flickr/Jobs/Queues/Owner.php
@@ -8,6 +8,13 @@ class Owner extends AbstractFlickrQueues
 {
     public function process(): bool
     {
+        /**
+         * If contact already exists, skip
+         */
+        if (Contact::where('nsid', $this->model->payload['nsid'])->exists()) {
+            return true;
+        }
+
         $owner = $this->service
             ->people()
             ->getInfo($this->model->payload['nsid']);
